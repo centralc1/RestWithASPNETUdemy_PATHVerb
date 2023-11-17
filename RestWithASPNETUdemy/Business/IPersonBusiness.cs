e96f53d71b110b9f004346f41f6e9edabefa6edd
Ricardo da Cunha <joaovitorleitesantos98@gmail.com>
2023-11-16 21:12:09 -0300

    Add project files.

---
+++ RestWithASPNETUdemy/Business/IPersonBusiness.cs
@@ -0,0 +1,16 @@
+﻿using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Business
+{
+    public interface IPersonBusiness
+    {
+        PersonVO Create(PersonVO person);
+        PersonVO FindByID(long id);
+        List<PersonVO> FindAll();
+        PersonVO Update(PersonVO person);
+        PersonVO Disable(long id);
+        void Delete(long id);
+    }
+}
